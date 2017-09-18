a9b3617524a65e4d46419a68fe2328560e20e193
gitknob <teterski@ualberta.ca>
2017-09-17 22:21:52 -0700

    Question 8 almost finished

--- lab2-8.c
+++ lab2-8.c
@@ -2,14 +2,10 @@
 
 #include <stdio.h>
 
-
-float * calcLoanPayments(
-	float loan, float interest, float monthly, int payments
-);
+int pow(int base, int exponent);
 
 int main(){
 
-	//Display with TWO decimal points!
 	int payments = 3;
 	float loan, interest, monthly;
 
@@ -17,28 +13,21 @@ int main(){
 	scanf("%f", &loan);
 	printf("Enter interest rate: ");
 	scanf("%f", &interest);
+	interest /= 100.0f * 12.0f;
 	printf("Enter monthly payment: ");
 	scanf("%f",&monthly);
-	
-	float *afterPayment = calcLoanPayments(loan, interest, monthly, payments);
-	
-	printf("Balance remaining after first payment: %f",  *(afterPayment + 0));
-	printf("Balance remaining after second payment: %f", *(afterPayment + 1));
-	printf("Balance remaining after third payment: %f",  *(afterPayment + 2));
+
+	float remaining[payments];
+
+	for(int p=0; p<payments; p++){
+		remaining[p] = loan - monthly;
+		remaining[p] += remaining[p] * interest;
+		loan = remaining[p];
+	}
+
+	printf("Balance remaining after  first payment: $%.2f\n", remaining[0]);
+	printf("Balance remaining after second payment: $%.2f\n", remaining[1]);
+	printf("Balance remaining after  third payment: $%.2f\n", remaining[2]);
 
 	return 0;
 }
-
-float * calcLoanPayments(
-	float loan, float interest, float monthly, int payments
-){
-
-	float afterPayment[payments];
-
-	for(int p=0; p<payments; p++){
-		afterPayment[p] = p;
-	}
-
-	return afterPayment;
-}
-
